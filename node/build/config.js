a04ac5b2ba1942e9ab3b48f806e5b0e94206f48b
ZXHzxh123 <zhang.xiaohui@timescreativity.com>
2019-03-13 13:01:24 +0800

    node 完成

---
+++ node/build/config.js
@@ -0,0 +1,103 @@
+const path = require('path')
+const merge = require('webpack-merge')
+
+function resolve(dir) {
+    return path.resolve(__dirname, '../', dir)
+}
+
+module.exports = {
+    entry: resolve('src/app.js'),
+    output: {
+        path: resolve('dist'),
+        filename: "js/layout.js"
+    },
+    module: {
+        rules: [{
+            test: /\.html$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: '[name].html'
+                }
+            }, {
+                loader: "extract-loader"
+            }, {
+                loader: "html-loader",
+                options: {
+                    attrs: ['data-src', 'link:href'],
+                    root: resolve('src/assets')
+                }
+            }]
+        }, {
+            test: /\.scss$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: 'css/[name].css'
+                }
+            }, {
+                loader: "extract-loader"
+            }, {
+                loader: "css-loader",
+                options: {
+                    importLoaders: 1
+                }
+            }, {
+                loader: "postcss-loader",
+                options: {
+                    ident: 'postcss',
+                    plugins: [
+                        require('postcss-sprites')(),
+                        require('autoprefixer')(),
+                        require('cssnano')()
+                    ]
+                }
+            }, {
+                loader: "sass-loader"
+            }]
+        }, {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-transform-runtime']
+                }
+            }, {
+                loader: "eslint-loader",
+            }]
+        }, {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            use: [{
+                loader: "url-loader",
+                options: {
+                    limit: 10000
+                }
+            }, {
+                loader: "img-loader",
+                options: {
+                    plugins: [
+                        require('imagemin-gifsicle')({
+                            interlaced: false
+                        }),
+                        require('imagemin-mozjpeg')({
+                            progressive: true,
+                            arithmetic: false
+                        }),
+                        require('imagemin-pngquant')({
+                            floyd: 0.5,
+                            speed: 2
+                        }),
+                        require('imagemin-svgo')({
+                            plugins: [
+                                { removeTitle: true },
+                                { convertPathData: false }
+                            ]
+                        })
+                    ]
+                }
+            }]
+        }]
+    }
+}
